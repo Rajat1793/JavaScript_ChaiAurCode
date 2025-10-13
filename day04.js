5d40093cb9a260c67c9ac7689101ef2b7fac84f9
Rajat Jaiswal <32808334+Rajat1793@users.noreply.github.com>
2025-10-13 14:46:47 +0000

    day 4

---
+++ day04.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // this wont work as we are using the nodejs not the browser
+
+console.log(3+3); // code readbility should be there
+
+// Data Types
+
+let name = "rajat"
+let age = 11
+let isLoggin = false
+
+// number =2 to power 53
+// bigint
+// string >""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null);  //object
+
