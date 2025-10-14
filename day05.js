95230960d44ee5fe3f2acb252a08eba0946fd4b1
Rajat Jaiswal <32808334+Rajat1793@users.noreply.github.com>
2025-10-14 04:38:29 +0000

    day 5

---
+++ day05.js
@@ -0,0 +1,22 @@
+let score = ""
+let names = 123
+// console.log(typeof score);
+
+let valueInnumber = Number(score) //converting the string into number
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+let valueInBool = Boolean(score)
+// console.log(valueInBool);
+
+let valueInString = String(names)
+console.log(typeof valueInString);
+console.log(valueInString);
+
+
+// "33" => 33
+// "33as" => NaN
+// true => 1 false => 0
+// 1 => true 
+// "" => false
+// "rajat" => true
