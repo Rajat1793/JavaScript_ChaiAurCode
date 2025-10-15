f18ea37332afe1e85c760c414cbb6a3184ed35d7
Rajat Jaiswal <32808334+Rajat1793@users.noreply.github.com>
2025-10-15 05:26:10 +0000

    day 6

---
+++ day06.js
@@ -0,0 +1,18 @@
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " rajat"
+let str3 =  str1 + str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2"); 
+
+console.log(+true);
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
