0ee7d8587497b52235fc48c7367b698fd1f1ef48
Rajat Jaiswal <32808334+Rajat1793@users.noreply.github.com>
2025-10-19 02:02:33 +0000

    day10

---
+++ day10.js
@@ -0,0 +1,37 @@
+ const name = "rajat"
+ const repocount = 24
+
+//  console.log(name + repocount + "value"); not a good and prefered way of writting
+
+console.log(`Hello my name is ${name} and i have ${repocount} repo`);
+
+const gameName = new String ("Formula1")
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('1'));
+
+const nameSlice = gameName.slice(3, 5)
+// console.log(nameSlice);
+
+const nameSlice1 = gameName.slice(-7,4)
+// console.log(nameSlice1);
+
+const subString = gameName.substring(3, 5)
+// console.log(subString);
+
+const newString = "      rajat.     "
+console.log(newString.trim());
+
+
+
+
+
+
+
+
