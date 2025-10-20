beec787478db5da7cacdde683682658bf3df5725
Rajat Jaiswal <32808334+Rajat1793@users.noreply.github.com>
2025-10-20 02:54:07 +0000

    day11

---
+++ day11.js
@@ -0,0 +1,34 @@
+// ######### Numbers ###########
+
+const score = 400
+const balance = new Number(100)
+
+// console.log(balance);
+// console.log(score);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const number2 = 324.75534324
+// console.log(number2.toPrecision(4));
+
+// const number3 = 10000000
+// console.log(number3.toLocaleString('en-IN'));
+
+// ############ Maths #############
+ console.log(Math);
+ console.log(Math.abs(-4));
+ console.log(Math.round(34.435));
+ console.log(Math.ceil(4.3));
+ console.log(Math.floor(4.9));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+min);
+
+ 
+ 
