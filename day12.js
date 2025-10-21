6963ef11b31baf480099f74b9c3a4fdcc3d135c8
Rajat Jaiswal <32808334+Rajat1793@users.noreply.github.com>
2025-10-21 04:36:10 +0000

    day12

---
+++ day12.js
@@ -0,0 +1,29 @@
+// Dates
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(typeof myDate);
+// console.log(myDate.toLocaleString());
+
+let myDate1 = new Date(2025, 9, 17)
+// console.log(myDate1.toLocaleDateString());
+// console.log(myDate1.toLocaleString());
+
+let myDate2 = new Date("2025-10-17")
+// console.log(myDate2.toLocaleString());
+
+let myDate3 = new Date("01-01-2023")
+// console.log(myDate3.toLocaleString())
+
+// Time
+
+let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(myDate3.getTime());
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() +1);
+
